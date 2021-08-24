5bd2b969a5db40edc4b9ee25ddc7610bae09c2ae
qcwgithub <doitmyway@qq.com>
2021-08-24 17:48:51 +0800

    tutorial

--- ScanToChat/ScanToChat.Shared/MainPage.xaml.cs
+++ ScanToChat/ScanToChat.Shared/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ScanToChat.Models;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,83 @@ namespace ScanToChat
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static readonly DependencyProperty IssueItemProperty =
+            DependencyProperty.Register(nameof(Item), typeof(IssueItem), typeof(MainPage), new PropertyMetadata(default(IssueItem)));
+
         public MainPage()
         {
+            Item = new IssueItem
+            {
+                Id = 1232,
+                Title = "Getting Started",
+                Description = @"Create a page to enter Issues that we need to work on.
+## Acceptance Criteria
+- Display the issue Id
+- Provide an ability to select the issue Type (i.e. Bug, Feature, etc)
+- Include an Issue Title
+- Include a full issue description with support for MarkDown
+- Include an issue effort
+- Include an ability for a developer to update the Status (i.e Icebox, WIP, etc)
+## Additional Comments
+We would like to have a visual indicator for the type of issue as well as something to visualize the effort involved",
+                Effort = 3,
+                Status = IssueStatus.WIP,
+                Type = IssueType.Feature,
+                CreatedAt = new DateTimeOffset(2019, 04, 03, 08, 0, 0, TimeSpan.FromHours(-8)),
+                StartedAt = new DateTimeOffset(2019, 04, 30, 08, 0, 0, TimeSpan.FromHours(-8))
+            };
+
             this.InitializeComponent();
+        }
+
+        public IssueItem Item
+        {
+            get => (IssueItem)GetValue(IssueItemProperty);
+            set => SetValue(IssueItemProperty, value);
+        }
+        public IssueStatus[] StatusList => new[]
+    {
+        IssueStatus.Icebox,
+        IssueStatus.Planned,
+        IssueStatus.WIP,
+        IssueStatus.Done,
+        IssueStatus.Removed
+    };
+
+        public IssueType[] IssueTypeList => new[]
+        {
+        IssueType.Bug,
+        IssueType.Feature,
+        IssueType.Issue,
+        IssueType.Task
+    };
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Item = new IssueItem
+            {
+                Id = 1232,
+                Title = "Getting Started",
+                Description = @"Create a page to enter Issues that we need to work on.
+
+## Acceptance Criteria
+
+- Display the issue Id
+- Provide an ability to select the issue Type (i.e. Bug, Feature, etc)
+- Include an Issue Title
+- Include a full issue description with support for Markdown
+- Include an issue effort
+- Include an ability for a developer to update the Status (i.e Icebox, WIP, etc)
+
+## Additional Comments
+
+We would like to have a visual indicator for the type of issue as well as something to visualize the effort involved",
+                Effort = 3,
+                Status = IssueStatus.WIP,
+                Type = IssueType.Feature,
+                CreatedAt = new DateTimeOffset(2019, 04, 03, 08, 0, 0, TimeSpan.FromHours(-8)),
+                StartedAt = new DateTimeOffset(2019, 04, 30, 08, 0, 0, TimeSpan.FromHours(-8))
+            };
         }
     }
 }
